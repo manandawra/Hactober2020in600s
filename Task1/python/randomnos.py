dcc702360858b352956eae187e491888e5c741cf
Manan Dawra <72313134+manandawra@users.noreply.github.com>
2020-10-17 18:41:34 +0530

    Updated
    Made some Changes.

--- Task1/python/randomnos.py
+++ Task1/python/randomnos.py
@@ -11,6 +11,6 @@ deck = list(itertools.product(range(1,14),['Spade','Heart','Diamond','Club']))
 random.shuffle(deck)
 
 # draw five cards
-print("Number you got")
+print("You got: ")
 for i in range(5):
    print(deck[i][0], "of", deck[i][1])
