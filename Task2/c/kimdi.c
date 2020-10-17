49ae93f6187b9bb1dbba115232b3ef9e0892563c
sk1384 <64589631+sk1384@users.noreply.github.com>
2020-10-17 17:50:27 +0530

    updated

--- Task2/c/kimdi.c
+++ Task2/c/kimdi.c
@@ -31,7 +31,7 @@ void printArray(int arr[], int size)
 // Driver program to test above functions 
 int main() 
 { 
-    int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
+    int arr[7] = {64, 34, 25, 12, 22, 11, 90}; 
     int n = sizeof(arr)/sizeof(arr[0]); 
     bubbleSort(arr, n); 
     printf("Sorted array: \n"); 
